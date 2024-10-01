32bec119ad5068e96819527a8b67f59a891ab146
omaima <benhlima.omaima.solicode@gmail.com>
2024-10-01 15:45:08 +0100

    adding responsive navbar code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-white'>hi </h1>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
