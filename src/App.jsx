117e3f1686b49f08836adc8953a6e7fc4b3f323f
omaima <benhlima.omaima.solicode@gmail.com>
2024-10-01 20:29:12 +0100

    add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
 const App = () => {
   return (
     <div>
       <h1 className='text-white'>hi </h1>
-      <Navbar/>
+      <Navbar />
+      <Hero/>
     </div>
   )
 }
